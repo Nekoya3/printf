92257320d2ae31b3f0d61cc84d128305d3a75c1a
Nekoya3 <anekoye3@gmail.com>
2022-10-24 01:15:36 +0300

    Team files

--- _write_char.c
+++ _write_char.c
@@ -1,14 +1,11 @@
-:set paste
 #include "main.h"
-
-
 
 /**
  * _write_char - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _write_char(char c)
 {
